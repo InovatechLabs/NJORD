f81c5cd66c81d9ca719cb662256d0d5092f70c5d
gjulianni <gabrieljulianni24@gmail.com>
2025-06-15 23:26:03 -0300

    fix

--- src/components/home/alert/Alert.tsx
+++ src/components/home/alert/Alert.tsx
@@ -263,16 +263,25 @@ const data = new Date(horarioOriginal);
      </div>
              <div className="text-white text-xl mt-4">
                   {dados?.reading_time && (
-                    <span>
-                      Última leitura:{" "}
-                      {new Intl.DateTimeFormat("pt-BR", {
-                        dateStyle: "short",
-                        hour: "2-digit",
-                        minute: "2-digit",
-                        hour12: false,
-                      }).format(data)}
-                    </span>
-                  )}
+  <span>
+    Última leitura:{" "}
+    {/* Data */}
+    {new Intl.DateTimeFormat("pt-BR", {
+      timeZone: "America/Sao_Paulo",
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    }).format(data)}{" "}
+    às{" "}
+    {/* Hora */}
+    {new Intl.DateTimeFormat("pt-BR", {
+      timeZone: "America/Sao_Paulo",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    }).format(data)}
+  </span>
+)}
           </div>
         </Card>
         </div>
